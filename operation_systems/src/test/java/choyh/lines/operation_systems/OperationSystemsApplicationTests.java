d7debfb04dfd13c8d3cf40d2a185806ea65519d5
CHOYH-PC\choyh <emfosvk@gmail.com>
2022-01-25 19:45:33 +0900

    operation system Prcatice 버전 추가

---
+++ operation_systems/src/test/java/choyh/lines/operation_systems/OperationSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package choyh.lines.operation_systems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperationSystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ sample/src/main/java/choyh/lines/sample/StarterConfiguration.java
@@ -0,0 +1,4 @@
+package choyh.lines.sample;
+
+public class StartConfiguration {
+}
